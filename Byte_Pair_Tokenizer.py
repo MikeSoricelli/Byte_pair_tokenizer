9755738a612301eb716dd9afc04f4ac30ea1c773
MikeSoricelli <69374345+MikeSoricelli@users.noreply.github.com>
2025-08-12 19:14:34 -0400

    Add files via upload

---
+++ Byte_Pair_Tokenizer.py
@@ -0,0 +1,35 @@
+import heapq
+
+def tokenize(text):
+    tokens = text
+    mapping = {}
+
+
+    #run byte pair encoding for until only 10,000 tokens remain
+    while len(tokens) > 10000:
+
+        max_token_count = 0
+        max_token = tokens[0]
+
+        #find candidate token with most occurances
+        for i in range(len(tokens) - 1):
+            candidate_token = tokens[i] + tokens[i+1]
+            if candidate_token in mapping:
+                mapping[candidate_token] += 1
+                if mapping[candidate_token] > max_token_count:
+                    max_token_count = mapping[candidate_token]
+                    max_token = candidate_token
+            else:
+                mapping[candidate_token] = 1
+
+        # replace all subcomponents of max token with max token
+
+        for i, token in enumerate(tokens):
+            if not(i >= (len(tokens) - 1)):
+                if tokens[i] + tokens[i+1] == max_token:
+                    tokens = tokens[:i] + tokens[i+1:]
+
+
+    return tokens
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+from text_cleaner import text_cleaner
+import argparse
+from Byte_Pair_Tokenizer import tokenize
+import os
+
+def main(fileName):
+
+    file = open(fileName, 'r')
+    text = file.read()
+    tokens = tokenize(text)
+    print(f"text length = {len(text)}, pair length = {len(tokens)}")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fileName', type=str, help='The file to be cleaned')
+    args = parser.parse_args()
+    main(args.fileName)
